ef42aab00298cb78dbe20487172f654286a04baf
Maksym Padalka <sheihmaks@gmail.com>
2022-05-20 01:18:45 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
@@ -7,16 +7,29 @@ const refs = {
   countryInfo: document.querySelector('.country-info'),
 };
 
-console.log(refs.input);
-fetch(URL)
-  .then(response => {
-    return response.json();
-  })
-  .then(pokemon => {
-    console.log(pockemon);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+// console.log(refs.input);
+// fetch(URL)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(pokemon => {
+//     console.log(pockemon);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
 
-refs.input.addEventListener('input', onSearchCountries);
+refs.input.addEventListener('input', debounce(onSearchCountries, DEBOUNCE_DELAY));
+
+function onSearchCountries(e) {
+  const countrySearch = e.target.value.trim();
+  if (!countrySearch) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
+  }
+  // onSearchCountry(countrySearch);
+  console.dir(countrySearch);
+}
+const URL = `https://restcountries.com/v3.1/name/{name}`;
+// function onSearchCountry
