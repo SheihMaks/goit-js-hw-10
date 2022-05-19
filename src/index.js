9bbcc932b27e96f52f1bc012655c200e534c9fa3
Maksym Padalka <sheihmaks@gmail.com>
2022-05-19 22:41:14 +0300

    +

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 const refs = {
   input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
 };
+
+console.log(refs.input);
+fetch(URL)
+  .then(response => {
+    return response.json();
+  })
+  .then(pokemon => {
+    console.log(pockemon);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+refs.input.addEventListener('input', onSearchCountries);
