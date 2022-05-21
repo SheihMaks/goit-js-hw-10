f385e23649fc0995a5349ec1df9a03869a5de0cd
Maksym Padalka <sheihmaks@gmail.com>
2022-05-22 01:38:48 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import { onSearchCountry } from './js/fetchCountries';
+import markupCountriesList from './templates/markupCountriesList.hbs';
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -26,11 +27,22 @@ function onSearchCountries(e) {
   onSearchCountry(country)
     .then(data => {
       if (data.length > 10) {
-        Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-      } else if (data.length <= 10 && data.length > 1) {
-        console.log();
-        refs.countryInfo.textContent = ')))';
+        Notiflix.Notify.success('Too many matches found. Please enter a more specific name.');
+      } else if (data.length > 1 && data.length <= 10) {
+        console.log(data);
+        refs.countryList.insertAdjacentHTML('afterbegin', markupCountriesList(data));
+      } else {
+        console.log(data);
+        // markupCountriesList(data);
       }
     })
     .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
 }
+
+// function markUp(data) {
+//   // const { name } = data;
+//   data.map(el => {
+//     console.log(el.name.official);
+//     return `<p>${el.name.official}</p>`;
+//   });
+// }
